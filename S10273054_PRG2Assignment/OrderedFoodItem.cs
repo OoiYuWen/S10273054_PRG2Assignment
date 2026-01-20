17a41c6230d514fc785ce151fe249a45ef259986
OoiYuWen <S10273054@connect.np.edu.sg>
2026-01-20 19:17:29 +0800

    Add project files.

---
+++ S10273054_PRG2Assignment/OrderedFoodItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10273054_PRG2Assignment
+{
+    internal class OrderedFoodItem
+    {
+    }
+}
