900a9e42594e55339b115c8582318faaf097e199
Studio-Ghibli <chloechanxin@gmail.com>
2026-01-22 11:45:10 +0800

    Implementation of OrderedFoodItem class

--- S10273054_PRG2Assignment/Order.cs
+++ S10273054_PRG2Assignment/Order.cs
@@ -11,7 +11,31 @@ namespace S10273054_PRG2Assignment
     // Student Name : Ooi Yu Wen
     // Partner Name : Chan Xin Chloe
     //========================================================== 
-    internal class Order
+    class Order
     {
+        // Attributes
+        public int OrderId { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public double OrderTotal { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime DeliveryDateTime { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string OrderPaymentMethod { get; set; }
+        public bool OrderPaid { get; set; }
+        public List<Customer> CustomerList { get; set; } = new List<Customer>();
+
+        // Constructor
+        public Order() { }
+        public Order(int orderId, DateTime orderDateTime, double orderTotal, string orderStatus, DateTime deliveryDateTime, string deliveryAddress, string orderPaymentMethod, bool orderPaid)
+        {
+            OrderId = orderId;
+            OrderDateTime = orderDateTime;
+            OrderTotal = orderTotal;
+            OrderStatus = orderStatus;
+            DeliveryDateTime = deliveryDateTime;
+            DeliveryAddress = deliveryAddress;
+            OrderPaymentMethod = orderPaymentMethod;
+            OrderPaid = orderPaid;
+        }
     }
 }
--- S10273054_PRG2Assignment/OrderedFoodItem.cs
+++ S10273054_PRG2Assignment/OrderedFoodItem.cs
@@ -11,7 +11,25 @@ namespace S10273054_PRG2Assignment
     // Student Name : Ooi Yu Wen
     // Partner Name : Chan Xin Chloe
     //========================================================== 
-    internal class OrderedFoodItem
+    class OrderedFoodItem : FoodItem
     {
+        // Attributes
+        public int QtyOrdered { get; set; }
+        public double SubTotal { get; set; }
+
+        // Constuctors
+        public OrderedFoodItem() { }
+        public OrderedFoodItem(string itemName, string itemDesc, double itemPrice, string customise, int qtyOrdered, double subTotal) : base(itemName, itemDesc, itemPrice, customise)
+        {
+            QtyOrdered = qtyOrdered;
+            SubTotal = subTotal;
+        }
+
+        // Methods
+        public double CalculateSubTotal()
+        {
+            SubTotal = ItemPrice * QtyOrdered;
+            return SubTotal;
+        }
     }
 }
