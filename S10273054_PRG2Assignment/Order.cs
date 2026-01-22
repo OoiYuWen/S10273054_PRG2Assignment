b6f6d097a905f1a72959403e15be95d501218ab1
Studio-Ghibli <chloechanxin@gmail.com>
2026-01-22 12:06:10 +0800

    Implementation of Order class

--- S10273054_PRG2Assignment/Order.cs
+++ S10273054_PRG2Assignment/Order.cs
@@ -13,6 +13,66 @@ namespace S10273054_PRG2Assignment
     //========================================================== 
     class Order
     {
-        
+        // Attributes
+        public int OrderId { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public double OrderTotal { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime DeliveryDateTime { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string OrderPaymentMethod { get; set; }
+        public bool OrderPaid { get; set; }
+        public List<OrderedFoodItem> OrderList { get; set; } = new List<OrderedFoodItem>();   // OrderedList
+
+        // Constructor
+        public Order() { }
+        public Order(int orderId, DateTime orderDateTime, double orderTotal, string orderStatus, DateTime deliveryDateTime, string deliveryAddress, string orderPaymentMethod, bool orderPaid)
+        {
+            OrderId = orderId;
+            OrderDateTime = orderDateTime;
+            OrderTotal = orderTotal;
+            OrderStatus = orderStatus;
+            DeliveryDateTime = deliveryDateTime;
+            DeliveryAddress = deliveryAddress;
+            OrderPaymentMethod = orderPaymentMethod;
+            OrderPaid = orderPaid;
+        }
+
+        // Methods
+        public double CalculateOrderTotal()
+        {
+            double total = 0;
+            foreach (OrderedFoodItem item in OrderList)
+            {
+                total += item.SubTotal;
+            }
+            return total;
+        }
+        public void AddOrderedFoodItem(OrderedFoodItem orderedFoodItem)
+        {
+            OrderList.Add(orderedFoodItem);
+        }
+        public bool RemoveOrderedFoodItem(OrderedFoodItem orderedFoodItem)
+        {
+            if (OrderList.Remove(orderedFoodItem))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void DisplayOrderedFoodItems()
+        {
+            foreach (OrderedFoodItem item in OrderList)
+            {
+                Console.WriteLine("Quantity ordered: " + item.QtyOrdered + "\tSubtotal: " + item.SubTotal);
+            }
+        }
+        public override string ToString()
+        {
+            return "Order ID: " + OrderId + "\nOrder Date and Time: " + OrderDateTime + "\nOrder Total: " + OrderTotal + "\nOrder Status: " + OrderStatus + "\nDelivery Date and Time: " + DeliveryDateTime + "\nDelivery Address: " + DeliveryAddress + "\nOrder Payment Method: " + OrderPaymentMethod + "\nOrder Paid: " + OrderPaid;
+        }
     }
 }
