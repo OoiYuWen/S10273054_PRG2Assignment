33d2c81cbfcd73e0b2109aaaeb44a25a3dcfa7e1
Studio-Ghibli <chloechanxin@gmail.com>
2026-01-23 22:36:59 +0800

    Implemented 1/2 of basic feature 2: Load Customer file

--- S10273054_PRG2Assignment/Program.cs
+++ S10273054_PRG2Assignment/Program.cs
@@ -3,3 +3,26 @@
 // Student Name : Ooi Yu Wen
 // Partner Name : Chan Xin Chloe
 //========================================================== 
+
+using S10273054_PRG2Assignment;
+// 2) Load files (customers and orders)
+List<Customer> custList = new List<Customer>();
+
+void LoadCustomers(List<Customer> customers)
+{
+    using (StreamReader sr = new StreamReader("customers.csv"))
+    {
+        string? s = sr.ReadLine(); // Skip header line
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] parts = s.Split(',');
+            string name = parts[0];
+            string email = parts[1];
+            Customer customer = new Customer(email, name);
+            custList.Add(customer);
+        }
+    }
+}
+
+
+
