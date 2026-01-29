f3305cb4f8e446a8a2d3e1a4b2a2dbff5c9fd071
Studio-Ghibli <chloechanxin@gmail.com>
2026-01-29 11:04:29 +0800

    Resolving commit conflicts and issues

--- S10273054_PRG2Assignment/Program.cs
+++ S10273054_PRG2Assignment/Program.cs
@@ -62,7 +62,7 @@ void LoadOrders(Dictionary<int, Order> orderList)
 }
 
 // 1) Load files (restaurants and food items) 
-Dictionary<string, Restaurant> restaurantDict = new Dictionary<string, Restaurant>(); 
+
 Dictionary<string, Menu> menus = new Dictionary<string, Menu>();
 List<FoodItem> fooditemlist = new List<FoodItem>();
 Dictionary<string, Restaurant> RestaurantDict =new Dictionary<string, Restaurant>(); 
